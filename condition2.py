00359435c5fd6019539805ec66be1a023915c8bf
PythonandLee <47309701+PythonandLee@users.noreply.github.com>
2019-02-08 20:38:26 -0400

    Add files via upload

---
+++ condition2.py
@@ -0,0 +1,10 @@
+# Make a list and ask Python for the answers: True/Flase. 
+fruit = ['apple', 'orange', 'grape', 'banana']
+if 'apple' in fruit:
+   print('apple' in fruit)
+   print("Nice guess, 'apple' is in fruit")
+# Also can use 'not in' to create a negtive condition.
+fruit_1 = 'kiwi'   
+if fruit_1 not in fruit:
+   print(fruit_1 not in fruit)
+   print("Nice, 'kiwi' is not in fruit.")
