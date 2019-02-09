c94d10a4e168ef25ea6560a74ac662c983f78377
PythonandLee <47309701+PythonandLee@users.noreply.github.com>
2019-02-08 20:34:37 -0400

    Delete condition2.py

--- condition2.py
+++
@@ -1,9 +0,0 @@
-fruit = ['apple', 'orange', 'grape', 'banana']
-if 'apple' in fruit:
-   print('apple' in fruit)
-   print("Nice guess, 'apple' is in fruit")
-
-fruit_1 = 'kiwi'   
-if fruit_1 not in fruit:
-   print(fruit_1 not in fruit)
-   print("Nice, 'kiwi' is not in fruit.")
